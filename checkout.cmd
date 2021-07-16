eb1b0b7ef69ef47eb687643e19415884e6174c0d
ep.komarov <ep.komarov@tensor.ru>
2021-07-16 11:58:49 +0500

    build

--- checkout.cmd
+++ checkout.cmd
@@ -6,12 +6,22 @@ set repos[0]=eo-engine
 set repos[1]=eo-core
 set repos[2]=fed-core
 set repos[3]=fed-documentformats
-set repos[4]=eo-np
-set repos[5]=eo-employees
-set repos[6]=sbis-sbis3controls
-set repos[7]=saby-wasaby-controls
-set repos[8]=eo-employees
-set reposCount=8
+
+set repos[4]=sbis-engine
+set repos[5]=sbis-sbis3controls
+set repos[6]=saby-wasaby-controls
+set repos[7]=saby-ui
+set repos[8]=sbis-rmi
+set repos[9]=saby-types
+set repos[10]=sbis-sdn
+set repos[11]=sbis-ws
+set repos[12]=saby-wasaby-app
+set repos[13]=saby-i18n
+set repos[14]=saby-wasaby-requirejs-loader
+set repos[15]=sbis-permission
+set repos[16]=saby-inferno
+
+set reposCount=16
 
 rem commands
 set comands[0]=all
@@ -22,11 +32,10 @@ set comands[4]=pl
 set comandsCount=4
 
 rem relations
-set all=0 1 2 3 4 5 6 7 8
-set rt=0 1 2 4 8
+set all=0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+set rt=0 1 2
 set df=3
-set eo=4 5
-set pl=6 7
+set pl=4 5 6 7 8 9 10 11 12 13 14 15 16
 
 echo Avaliable repositories:
 for /L %%r in (0, 1, %reposCount%) do (
